c12cb5cc8e9bc7e639c527505c5b9db90c9563f8
asifbuet047 <asifbuet047@gmail.com>
2025-05-07 01:01:31 +0600

    final submission

--- project_4.php
+++ project_4.php
@@ -1,17 +1,21 @@
 <?php
 
 echo "Please input Your electricity unit: ";
-$unit = (int)readline();
-if ($unit <= 50) {
-    $bill = $unit * 3.5;
-    echo "Your electricity bill is $bill taka";
-} elseif ($unit <= 100) {
-    $bill = 50 * 3.5 + ($unit - 50) * 4.0;
-    echo "Your electricity bill is $bill taka";
-} elseif ($unit <= 200) {
-    $bill = 50 * 3.5 + 50 * 4.0 + ($unit - 100) * 5.20;
-    echo "Your electricity bill is $bill taka";
+$unit = (float)readline();
+if ($unit >= 0) {
+    if ($unit <= 50) {
+        $bill = $unit * 3.5;
+        echo sprintf("Your electricity bill is %.2f taka", $bill);
+    } elseif ($unit <= 100) {
+        $bill = 50 * 3.5 + ($unit - 50) * 4.0;
+        echo sprintf("Your electricity bill is %.2f taka", $bill);
+    } elseif ($unit <= 200) {
+        $bill = 50 * 3.5 + 50 * 4.0 + ($unit - 100) * 5.20;
+        echo sprintf("Your electricity bill is %.2f taka", $bill);
+    } else {
+        $bill = 50 * 3.5 + 50 * 4.0 + 100 * 5.20 + ($unit - 200) * 6.50;
+        echo sprintf("Your electricity bill is %.2f taka", $bill);
+    }
 } else {
-    $bill = 50 * 3.5 + 50 * 4.0 + 100 * 5.20 + ($unit - 200) * 6.50;
-    echo "Your electricity bill is $bill taka";
+    echo "Please input a valid unit";
 }
